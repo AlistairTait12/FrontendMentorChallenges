f45555adf05656e96fa3ee69fea3e8b36e2d503c
Alistair Tait <alistair.tait1@gmail.com>
2023-04-16 21:25:51 +0100

    Added star icon in top corner of component, added translucency to rating labels, added content to rating labels

---
+++ rating-component/app.js
@@ -0,0 +1,4 @@
+class RatingComponent {
+  // TODO: constructor sets event listeners? Or should there be a page handler which creates this
+  // class and sets the event listeners? Remember, it should do as little as possible.
+}
